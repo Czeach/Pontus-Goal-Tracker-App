19d325c3a9f974c271789a9228e54e7387f61345
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-09-24 14:42:33 +0100

    Create SignIn.java

---
+++ app/src/main/java/com/project/pontusgoaltracker/SignIn.java
@@ -0,0 +1,37 @@
+package com.project.pontusgoaltracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class SignIn extends AppCompatActivity {
+    TextView sign_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sign_up = findViewById(R.id.sign);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
