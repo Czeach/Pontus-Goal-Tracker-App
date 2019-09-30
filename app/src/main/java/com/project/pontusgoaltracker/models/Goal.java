df7246b9a05c163ff5c8cb2c5a157fe681d84a3c
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-09-30 20:48:54 +0100

    Update Goal.java

--- app/src/main/java/com/project/pontusgoaltracker/models/Goal.java
+++ app/src/main/java/com/project/pontusgoaltracker/models/Goal.java
@@ -5,16 +5,32 @@ import java.util.Date;
 import java.util.UUID;
 
 public class Goal {
+    //keys :do not delete
+    public static final String GOAL_TITLE="GOAL_TITLE";
+    public static final String GOAL_DESCRIPTION="GOAL_DESCRIPTION";
+    public static final String GOAL_TYPE="GOAL_TYPE";
+    public static final String IS_COMPLETED ="IS_COMPLETED";
+    public static final String DEADLINE="DEADLINE";
+    public static final String TASKS="TASKS";
+
+
+
+
 
     private UUID goalId ;
+    private String goalIdString;
     private String title;
     private String description;
     private Date dateCreated;
-    private Date deadline;
+    private String deadline;
     private Date dateCompleted;
     private boolean isCompleted;
     private String goalType;
-    private ArrayList<Task> tasks;
+    private ArrayList<Task> tasks =new ArrayList<Task>();
+    private int completedTaskCount = 0;
+    private int taskSize;
+
+
 
     //todo : add id to goal
     //todo : create setDeadline method
@@ -25,11 +41,15 @@ public class Goal {
     //constructors
     //warning : do not use this constructor. do not delete either .
     //No constructor sets the deadline of Goal.. explicitly call setDeadline after creating an object.
-    public Goal(){}
+    public Goal(){
+
+        this.tasks = new ArrayList<Task>();
+    }
 
     public Goal(String title){
         this.dateCreated = new Date();
         this.goalId = UUID.randomUUID();
+        this.goalIdString=goalId.toString();
         this.title = title;
         this.tasks = new ArrayList<Task>();
     }
@@ -37,6 +57,7 @@ public class Goal {
     public Goal(String title, String description) {
         this.dateCreated = new Date();
         this.goalId = UUID.randomUUID();
+        this.goalIdString=goalId.toString();
         this.title = title;
         this.description = description;
         this.goalType = GoalType.GENERAL;
@@ -46,6 +67,7 @@ public class Goal {
     public Goal(String title, String description, String type) {
         this.dateCreated = new Date();
         this.goalId = UUID.randomUUID();
+        this.goalIdString=goalId.toString();
         this.title = title;
         this.description = description;
         this.goalType = type;
@@ -55,6 +77,7 @@ public class Goal {
     public Goal(String title, String description, String type, ArrayList<Task> tasks) {
         this.dateCreated = new Date();
         this.goalId = UUID.randomUUID();
+        this.goalIdString=goalId.toString();
         this.title = title;
         this.description = description;
         this.goalType = type;
@@ -78,11 +101,26 @@ public class Goal {
         isCompleted = completed;
     }
 
+    public void setCompletedTaskCount(int completedTaskCount) {
+        this.completedTaskCount = completedTaskCount;
+    }
+    public void setTaskSize(int taskSize) {
+        this.taskSize = taskSize;
+    }
+
     public void setType(String type) {
         this.goalType = type;
     }
 
-    public void setDeadline(Date deadline) {
+    public void emptyTasks(){
+        tasks=new ArrayList<>();
+    }
+
+//    public void setGoalIdString(String goalIdString) {
+//        goalIdString = goalIdString;
+//    }
+
+    public void setDeadline(String deadline) {
         this.deadline = deadline;
     }
 
@@ -104,7 +142,7 @@ public class Goal {
         return dateCompleted;
     }
 
-    public Date getDeadline() {
+    public String getDeadline() {
         return deadline;
     }
 
@@ -112,12 +150,23 @@ public class Goal {
 //        return goalId;
 //    }
     public String getGoalIdString() {
-        return goalId.toString();
+        return goalIdString;
     }
 
+
     public boolean isCompleted() {
+        if(calculatePercentageComplete()>=100)return true;
         return isCompleted;
     }
+
+    public int getCompletedTaskCount() {
+        return completedTaskCount;
+    }
+    public int getTaskSize() {
+        return taskSize;
+    }
+
+
 
     public String getType() {
         return goalType;
@@ -130,7 +179,24 @@ public class Goal {
     //append
 
     public void addTask(Task task){
+        if(task.isCompleted()){
+            completedTaskCount++;
+        }
         tasks.add(task);
+    }
+    public void deleteTask(Task task){
+        if(task.isCompleted()){
+            completedTaskCount--;
+        }
+        tasks.remove(task);
+    }
+    public double calculatePercentageComplete(){
+
+        double percentage  = 0.00;
+        //prevents divide by zero error in cases of empty task lists
+            if(taskSize>0){ percentage =(Double.valueOf(completedTaskCount)/Double.valueOf(taskSize)*100);
+            }
+        return percentage;
     }
 
 }
